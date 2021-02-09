428de2706069206eca672c94f2ebe1b4b45a5f49
Ben Lambert <46206905+benlambert43@users.noreply.github.com>
2021-02-09 16:10:24 -0500

    adding the client driver functionality to communicate over serial

--- clientDriver/clientDriver.py
+++ clientDriver/clientDriver.py
@@ -1,6 +1,9 @@
+# Importing Libraries
 import serial
-import time
 
+arduino = serial.Serial(port='COM3', baudrate=9600, timeout=100)
+def write(x):
+    arduino.write(bytes(x+'\n', 'utf-8'))
 
-
-arduinoSerialBand = serial.Serial()
+injection = input("Send code to Arduino Client: ") # Taking input from user
+value = write(injection)
