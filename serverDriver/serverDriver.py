267f98c05e204aea00ecab683a62aa73f772488b
Ben Lambert <46206905+benlambert43@users.noreply.github.com>
2021-03-23 18:45:37 -0400

    comment

--- serverDriver/serverDriver.py
+++ serverDriver/serverDriver.py
@@ -4,12 +4,15 @@ import mysqlSetup
 import time
 
 if __name__ == "__main__":
+    # Run setup once
     mysqlSetup.mysqlinitsetup()
 
     # creating thread
     t1 = threading.Thread(target=arduinoConnection.arduinoEngine)
     t1.daemon = True
-    # starting thread 1
     t1.start()
+
+
+    # keep daemon threads alive
     while True:
         time.sleep(1)
